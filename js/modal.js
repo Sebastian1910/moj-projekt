45e9fedbf417bdd0eff398e74e686cfa8a9b9dd2
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-01-22 23:25:46 +0100

    zzz

--- js/modal.js
+++ js/modal.js
@@ -27,3 +27,18 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[c-modal-open]'),
+    closeModalBtn: document.querySelector('[c-modal-close]'),
+    modal: document.querySelector('[c-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
