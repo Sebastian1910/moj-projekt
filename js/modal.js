45ef35501acb59be0d337e9e85a5823c4dfb5e9b
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-01-23 22:56:34 +0100

    z

--- js/modal.js
+++ js/modal.js
@@ -1,81 +1,81 @@
-// Modal 1
+// Modal contact
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector("[contact-modal-open]"),
+    closeModalBtn: document.querySelector("[contact-modal-close]"),
+    modal: document.querySelector("[contact-data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
 
-// Modal 2
+// Modal follow
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[s-modal-open]'),
-    closeModalBtn: document.querySelector('[s-modal-close]'),
-    modal: document.querySelector('[s-modal]'),
+    openModalBtn: document.querySelector("[follow-modal-open]"),
+    closeModalBtn: document.querySelector("[follow-modal-close]"),
+    modal: document.querySelector("[follow-data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
 
-// Modal 3
+// Modal thank
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[c-modal-open]'),
-    closeModalBtn: document.querySelector('[c-modal-close]'),
-    modal: document.querySelector('[c-modal]'),
+    openModalBtn: document.querySelector("[thank-modal-open]"),
+    closeModalBtn: document.querySelector("[thank-modal-close]"),
+    modal: document.querySelector("[thank-data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
 
 // Menu modalne
 
 (() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const mobileMenu = document.querySelector(".js-menu-container");
+  const openMenuBtn = document.querySelector(".js-open-menu");
+  const closeMenuBtn = document.querySelector(".js-close-menu");
 
   const toggleMenu = () => {
     const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
 
     const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
+      ? "disableBodyScroll"
+      : "enableBodyScroll";
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
